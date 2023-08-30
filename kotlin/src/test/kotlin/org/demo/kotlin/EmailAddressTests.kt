9e7b7b6fd2fe58c726100f159568711581b2c3ee
Andreas Huth <andreashuthe@t-online.de>
2023-08-30 16:29:49 +0200

    add first tests move kotlin to package

--- kotlin/src/test/kotlin/org/demo/kotlin/EmailAddressTests.kt
+++ kotlin/src/test/kotlin/org/demo/kotlin/EmailAddressTests.kt
@@ -1,12 +1,14 @@
 package org.demo.kotlin
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.fail
 
 class EmailAddressTests {
     @Test
     fun parsing() {
-        Assertions.assertEquals(
+        assertEquals(
             EmailAddress("fred", "example.com"),
             EmailAddress.parse("fred@example.com")
         )
@@ -14,8 +16,6 @@ class EmailAddressTests {
 
     @Test
     fun parsingFailures() {
-        Assertions.assertThrows(
-            IllegalArgumentException::class.java
-        ) { EmailAddress.parse("@") }
+        assertFails { EmailAddress.parse("@") }
     }
 }
